83b69de96e36b9eec725db93529ee8f58244eefe
solimander <85371573+solimander@users.noreply.github.com>
2022-02-07 10:59:21 -0700

    Populate template

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,79 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { PNGCollectionEncoder, PngImage } from '@nouns/sdk';
+import { promises as fs } from 'fs';
+import core from '@actions/core';
+import { PNG } from 'pngjs';
+import { join } from 'path';
 
-async function run(): Promise<void> {
+/**
+ * Get all directory names within `source`
+ * @param source The source directory
+ */
+const getDirectories = async (source: string) => {
+  const dir = await fs.readdir(source, { withFileTypes: true });
+  return dir.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+};
+
+/**
+ * Read a PNG image file and return a `PngImage` object.
+ * @param path The path to the PNG file
+ */
+const readPngImage = async (path: string): Promise<PngImage> => {
+  const buffer = await fs.readFile(path);
+  const png = PNG.sync.read(buffer);
+
+  return {
+    width: png.width,
+    height: png.height,
+    rgbaAt: (x: number, y: number) => {
+      const idx = (png.width * y + x) << 2;
+      const [r, g, b, a] = [png.data[idx], png.data[idx + 1], png.data[idx + 2], png.data[idx + 3]];
+      return {
+        r,
+        g,
+        b,
+        a,
+      };
+    },
+  };
+};
+
+/**
+ * Run the Github action, converting all PNG image data into a single RLE output file.
+ */
+const run = async (): Promise<void> => {
+  const encoder = new PNGCollectionEncoder();
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const outputPath = core.getInput('outputPath');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const rootPath = core.getInput('rootDirectoryPath');
+    const partFolders = await getDirectories(rootPath);
 
-    core.setOutput('time', new Date().toTimeString())
+    for (const folder of partFolders) {
+      const folderPath = join(rootPath, folder);
+      const files = await fs.readdir(folderPath);
+      for (const file of files) {
+        const image = await readPngImage(join(folderPath, file));
+        encoder.encodeImage(file.replace(/\.png$/, ''), image, folder.replace(/^\d-/, ''));
+      }
+    }
+
+    await fs.writeFile(
+      outputPath,
+      JSON.stringify(
+        {
+          bgcolors: ['d5d7e1', 'e1d7d5'],
+          ...encoder.data,
+        },
+        null,
+        2,
+      ),
+    );
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
-}
+};
 
-run()
+run();
